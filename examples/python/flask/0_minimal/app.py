54d3102cb41b45a43cfb875ebe5138b6bac71173
Leander Jehl <leanderjehl@yahoo.no>
2021-03-02 09:00:34 +0100

    fix

--- examples/python/flask/0_minimal/app.py
+++ examples/python/flask/0_minimal/app.py
@@ -2,7 +2,7 @@
 Flask: A minimal web application
 """
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -10,10 +10,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
-
-@app.route("/test")
-def test():
-    return render_template("index.html", msg="my message", value=123, id="myid", color="lightblue")
 
 if __name__ == "__main__":
     app.run()
